04456793aa57e25e7ee46d9461baa4f3baf28a67
devipriyamuvva <devipriyamuvva@gmail.com>
2024-07-24 22:39:02 +0530

    modified:   src/app/app.component.html 	modified:   src/app/forgotpwd/forgotpwd.component.html 	modified:   src/app/forgotpwd/forgotpwd.component.ts

--- src/app/forgotpwd/forgotpwd.component.ts
+++ src/app/forgotpwd/forgotpwd.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component,EventEmitter,Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forgotpwd',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './forgotpwd.component.html',
   styleUrl: './forgotpwd.component.css'
 })
 export class ForgotpwdComponent {
+  username=""
+  password=""
+  conpwd=""
+  userinfo:any=localStorage.getItem("Details")
+  userDetails:any[]=JSON.parse(this.userinfo)
 
+  @Output()
+  buttonClicked:EventEmitter<string>=new EventEmitter<string>();
+
+  changePwd(){
+    for(var i=0;i<this.userDetails.length;i++){
+      if(this.userDetails[i].username===this.username){
+        this.userDetails[i].password=this.password;
+        localStorage.setItem("Details",JSON.stringify(this.userDetails))
+        this.buttonClicked.emit("Login")
+        window.alert("Password Changed Successfully !!")
+        this.username=""
+        this.password=""
+        this.conpwd=""
+        break;
+      }
+    }
+  }
 }
